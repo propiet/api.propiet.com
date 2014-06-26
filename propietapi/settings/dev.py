6a3948577addfd874de8b7c06907371699ad4dda
poli <poli@devartis.com>
2014-06-26 13:05:27 -0300

    Add logging for all and I want MySQL

--- propietapi/settings/dev.py
+++ propietapi/settings/dev.py
@@ -2,4 +2,27 @@
 
 from settings.base import *
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console', ],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    }
+}
+
 __author__ = 'poli'
--- propietapi/settings/dev_poli.py
+++ propietapi/settings/dev_poli.py
@@ -2,4 +2,17 @@
 
 from settings.dev import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'propiet_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            "init_command": "SET unique_checks=0;",
+        },
+    }
+}
 __author__ = 'poli'
